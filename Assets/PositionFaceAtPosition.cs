9c19a80f4b01219c440bae0a32778db849ddc17a
guilschaidhauer <guilherme.schaidhauer.de.castro@sap.com>
2017-10-28 02:21:01 -0200

    Add small adjustments

--- Assets/PositionFaceAtPosition.cs
+++ Assets/PositionFaceAtPosition.cs
@@ -2,15 +2,25 @@
 
 public class PositionFaceAtPosition : MonoBehaviour
 {
+    public float limit = 65;
     private bool rotating = true;
 
     public void Update()
     {
         Vector3 pos = new Vector3(OpenCVFaceDetection.NormalizedFacePosition.x, OpenCVFaceDetection.NormalizedFacePosition.y, OpenCVFaceDetection.NormalizedFacePosition.z);
         //Vector3 newPos = new Vector3(pos.z * -1, 0, 0);
-        //Vector3 newPos2 = new Vector3(0, 0, pos.y * -1);
+        //Vector3 newPos = new Vector3(0, 0, pos.y);
 
-        Vector3 newPos = new Vector3(pos.z * -1, 0, pos.y * -1);
+        ///Vector3 newPos = new Vector3(pos.z * -1, 0, pos.y * -1);
+
+        //Vector3 newPos = new Vector3(pos.z, pos.x, pos.y);
+
+        if (pos.y > limit)
+            pos.y = limit;
+        else if (pos.y < -limit)
+            pos.y = -limit;
+
+        Vector3 newPos = new Vector3(pos.z, pos.x, pos.y);
 
         transform.localRotation = Quaternion.Euler(newPos);
     }
