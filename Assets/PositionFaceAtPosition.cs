9c655ac8079c54f8030449833b9503c2c8fc382e
guilschaidhauer <guilherme.schaidhauer.de.castro@sap.com>
2017-10-28 00:01:08 -0200

    Two axis rotation

--- Assets/PositionFaceAtPosition.cs
+++ Assets/PositionFaceAtPosition.cs
@@ -2,30 +2,16 @@
 
 public class PositionFaceAtPosition : MonoBehaviour
 {
-    public bool smooth = true;
-    public float smoothTime = 0.1F;
-    public Vector3 velocity = Vector3.zero;
+    private bool rotating = true;
 
-    private float _camDistance;
-
-    public Transform target;
-    public float speed;
-
-    void Start()
-    {
-        Application.runInBackground = true;
-        _camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
-    }
-
-    void Update()
+    public void Update()
     {
         Vector3 pos = new Vector3(OpenCVFaceDetection.NormalizedFacePosition.x, OpenCVFaceDetection.NormalizedFacePosition.y, OpenCVFaceDetection.NormalizedFacePosition.z);
-        //Vector3 pos = new Vector3(OpenCVFaceDetection.NormalizedFacePosition.y, OpenCVFaceDetection.NormalizedFacePosition.x, OpenCVFaceDetection.NormalizedFacePosition.z);
+        //Vector3 newPos = new Vector3(pos.z * -1, 0, 0);
+        //Vector3 newPos2 = new Vector3(0, 0, pos.y * -1);
 
+        Vector3 newPos = new Vector3(pos.z * -1, 0, pos.y * -1);
 
-        //Vector3 pos = new Vector3(OpenCVFaceDetection.NormalizedFacePosition.z * -1, 0, 26);
-
-
-        transform.rotation = Quaternion.Euler(pos);
+        transform.localRotation = Quaternion.Euler(newPos);
     }
 }
