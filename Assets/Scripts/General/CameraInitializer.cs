aa01ba5881df53d647405caaba55fec5babffedf
Schaidhauer De Castro <guilherme.schaidhauer.de.castro@sap.com>
2018-03-08 09:39:07 -0300

    Add exe file and execute it at start-up

---
+++ Assets/Scripts/General/CameraInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInitializer : MonoBehaviour {
+
+	void Start ()
+    {
+        StartCameraCapture();        
+    }
+
+    void StartCameraCapture ()
+    {
+        System.Diagnostics.Process.Start("Sockets.exe");
+    }
+}
