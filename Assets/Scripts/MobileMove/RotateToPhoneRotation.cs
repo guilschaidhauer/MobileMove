35b9f169d86c29d8d180a45fd17236860d2f4120
Schaidhauer De Castro <guilherme.schaidhauer.de.castro@sap.com>
2018-03-11 17:27:45 -0300

    Add acceleration tentative

--- Assets/Scripts/MobileMove/RotateToPhoneRotation.cs
+++ Assets/Scripts/MobileMove/RotateToPhoneRotation.cs
@@ -6,15 +6,15 @@ public class RotateToPhoneRotation : MonoBehaviour
     public float adjustRate;
     public float adjustInterval;
     public float limit = 65;
+    public bool weird = false;
+    public Transform other;
     private bool rotating = true;
 
     private float cumulativeAdjustment;
 
-    private float lastCheckpoint;
-    private bool hitLimit;
-    private bool downLeft;
+    float acceleration;
 
-    public void Start ()
+    public void Start()
     {
         if (adjust)
         {
@@ -31,22 +31,35 @@ public class RotateToPhoneRotation : MonoBehaviour
     {
         Vector3 pos = new Vector3(MobileDetection.NormalizedFacePosition.x, MobileDetection.NormalizedFacePosition.y, MobileDetection.NormalizedFacePosition.z);
 
-        float acceleration = pos.x;
+        if (pos.x <= 15000)
+        {
+            acceleration = 1;
+            pos.x = pos.x - 10000;
+        }
+        else {
+            acceleration = 2;
+            pos.x = pos.x - 20000;
+        }
 
-        if (acceleration > 0.5f)
+        if (weird)
         {
-            Debug.Log("->");
+            pos.x = acceleration;
         }
-        else if (acceleration < -0.5f)
+        else if (!weird && other)
         {
-            Debug.Log("<-");
+            //pos.y = 360 - other.localRotation.eulerAngles.z;
+            //pos.y *= -1;
+            //pos.y = other.localRotation.eulerAngles.z;
         }
+
+        Debug.Log(acceleration);
+        Debug.Log(pos.z);
 
         if (adjust)
             pos.x += cumulativeAdjustment;
 
-        //Vector3 newPos = new Vector3(pos.z, pos.x, pos.y);
-        Vector3 newPos = new Vector3(0, 0, pos.y);
+        Vector3 newPos = new Vector3(pos.z, pos.x, pos.y);
+        //Vector3 newPos = new Vector3(0, 0, pos.y);
 
         //Debug.Log(newPos.z);
 
